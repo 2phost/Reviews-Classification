2b02131fa77d69199054b650d21c5fb298342bbc
enesuysal <enesuysal@192.168.43.19>
2013-11-04 20:06:50 +0000

    Completed

--- RGI/src/RGI/RGILearner.java
+++ RGI/src/RGI/RGILearner.java
@@ -1,11 +1,18 @@
 package RGI;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Random;
 
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.meta.FilteredClassifier;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader.ArffReader;
+import weka.filters.unsupervised.attribute.StringToWordVector;
 
 public class RGILearner {
 
@@ -17,6 +24,14 @@ public class RGILearner {
      * Object that stores training data.
      */
     Instances trainData;
+    /**
+     * Object that stores the filter
+     */
+    StringToWordVector filter;
+    /**
+     * Object that stores the classifier
+     */
+    FilteredClassifier classifier;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RGILearner learner;
@@ -25,7 +40,7 @@ public class RGILearner {
         else {
                 learner = new RGILearner();
                 learner.loadDataset(args[0]);
-                // Evaluation mus be done before training
+                // Evaluation must be done before training
                 // More info in: http://weka.wikispaces.com/Use+WEKA+in+your+Java+code
                 learner.evaluate();
                 learner.learn();
@@ -50,6 +65,68 @@ public class RGILearner {
             }
             catch (IOException e) {
                     System.out.println("Problem found when reading: " + fileName);
+            }
+    }
+    
+    /**
+     * This method evaluates the classifier. As recommended by WEKA documentation,
+     * the classifier is defined but not trained yet. Evaluation of previously
+     * trained classifiers can lead to unexpected results.
+     */
+    public void evaluate() {
+            try {
+                    trainData.setClassIndex(0);
+                    filter = new StringToWordVector();
+                    filter.setAttributeIndices("last");
+                    classifier = new FilteredClassifier();
+                    classifier.setFilter(filter);
+                    classifier.setClassifier(new NaiveBayes());
+                    Evaluation eval = new Evaluation(trainData);
+                    eval.crossValidateModel(classifier, trainData, 4, new Random(1));
+                    System.out.println(eval.toSummaryString());
+                    System.out.println(eval.toClassDetailsString());
+                    System.out.println("===== Evaluating on filtered (training) dataset done =====");
+            }
+            catch (Exception e) {
+                    System.out.println("Problem found when evaluating");
+            }
+    }
+    
+    /**
+     * This method trains the classifier on the loaded dataset.
+     */
+    public void learn() {
+            try {
+                    trainData.setClassIndex(0);
+                    filter = new StringToWordVector();
+                    filter.setAttributeIndices("last");
+                    classifier = new FilteredClassifier();
+                    classifier.setFilter(filter);
+                    classifier.setClassifier(new NaiveBayes());
+                    classifier.buildClassifier(trainData);
+                    // Uncomment to see the classifier
+                    // System.out.println(classifier);
+                    System.out.println("===== Training on filtered (training) dataset done =====");
+            }
+            catch (Exception e) {
+                    System.out.println("Problem found when training");
+            }
+    }
+    
+    /**
+     * This method saves the trained model into a file. This is done by
+     * simple serialization of the classifier object.
+     * @param fileName The name of the file that will store the trained model.
+     */
+    public void saveModel(String fileName) {
+            try {
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName));
+        out.writeObject(classifier);
+        out.close();
+                     System.out.println("===== Saved model: " + fileName + " =====");
+    }
+            catch (IOException e) {
+                    System.out.println("Problem found when writing: " + fileName);
             }
     }
 
